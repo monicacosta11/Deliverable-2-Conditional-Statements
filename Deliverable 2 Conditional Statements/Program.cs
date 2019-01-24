c8413ded2ceacc864d987744673ac77668d3e2b3
Monica Costa <monicacosta@usf.edu>
2019-01-24 17:04:21 -0500

    Update

--- Deliverable 2 Conditional Statements/Program.cs
+++ Deliverable 2 Conditional Statements/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ Author: Monica Costa
+ Date: 1/22/2019
+ Comments: This C# Console application code demonstrates the use of 
+           conditional statements after getting grade input from users
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +17,93 @@ namespace Deliverable_2_Conditional_Statements
     {
         static void Main(string[] args)
         {
+            //Ask the user for input
+            Console.WriteLine("Please enter the grade you expect to get in ISM 4300...");
+
+            //Use try catch to catch bad user input
+            try
+            {
+                //// To get user input
+                string input = Console.ReadLine();
+                int letterGrade = int.Parse(input);
+
+                if (letterGrade >= 98 && letterGrade <= 100)
+                {
+                    Console.WriteLine("A+");
+                    Console.ReadKey(true);
+                }
+
+                else if (letterGrade >= 92 && letterGrade <= 97)
+                {
+                    Console.WriteLine("A");
+                    Console.ReadKey(true);
+                }
+
+                else if (letterGrade >= 90 && letterGrade <= 91)
+                {
+                    Console.WriteLine("A-");
+                    Console.ReadKey(true);
+                }
+                else if (letterGrade >= 88 && letterGrade <= 89)
+                {
+                    Console.WriteLine("B+");
+                    Console.ReadKey(true);
+                }
+                else if (letterGrade >= 82 && letterGrade <= 87)
+                {
+                    Console.WriteLine("B");
+                    Console.ReadKey(true);
+                }
+                else if (letterGrade >= 80 && letterGrade <= 81)
+                {
+                    Console.WriteLine("B-");
+                    Console.ReadKey(true);
+                }
+                else if (letterGrade >= 78 && letterGrade <= 79)
+                {
+                    Console.WriteLine("C+");
+                    Console.ReadKey(true);
+                }
+                else if (letterGrade >= 72 && letterGrade <= 77)
+                {
+                    Console.WriteLine("C");
+                    Console.ReadKey(true);
+                }
+                else if (letterGrade >= 70 && letterGrade <= 71)
+                {
+                    Console.WriteLine("C-");
+                    Console.ReadKey(true);
+                }
+                else if (letterGrade >= 68 && letterGrade <= 69)
+                {
+                    Console.WriteLine("D+");
+                    Console.ReadKey(true);
+                }
+                else if (letterGrade >= 62 && letterGrade <= 67)
+                {
+                    Console.WriteLine("D");
+                    Console.ReadKey(true);
+                }
+                else if (letterGrade >= 60 && letterGrade <= 61)
+                {
+                    Console.WriteLine("D-");
+                    Console.ReadKey(true);
+                }
+                else if (letterGrade >= 59 && letterGrade <= 0)
+                {
+                    Console.WriteLine("F");
+                    Console.ReadKey(true);
+                }
+
+            }
+
+            catch
+            {
+                Console.WriteLine("There was an error");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey(true);
+            } // End of catch
+
         }
     }
 }
